d54f078c43e11920ce205735d4e83fae66092958
Julian Andres Klode <jak@debian.org>
2018-02-26 17:50:24 +0100

    New upstream version 2.3.0~beta1+dfsg.1

--- src/version.h.cmake
+++ src/version.h.cmake
@@ -1,5 +1,5 @@
 #define GIT_HEAD "@GIT_HEAD@"
 #define GIT_DESCRIBE "@GIT_DESCRIBE@"
 
-#define DIST_HASH "4723f66d23db95f8318f04ee3aeae448d2305c9c"
-#define DIST_DATE "1513187936"
+#define DIST_HASH "10bd73cd4a0cc7af9ea36fc53fdf3106fd091f89"
+#define DIST_DATE "1519238581"
